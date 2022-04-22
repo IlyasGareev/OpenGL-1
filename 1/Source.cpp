3db372a2d63d8c642889f54defa2fb8610e1a97d
Ilyas <gareev_ilyas@list.ru>
2022-04-22 20:50:30 +0500

    комментарии

--- 1/Source.cpp
+++ 1/Source.cpp
@@ -1,31 +1,29 @@
 #include <GL/freeglut.h>
 
-
 static void RenderSceneCB()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
+	glClear(GL_COLOR_BUFFER_BIT);// очистка буфера кадра
+	glutSwapBuffers();//функция, которая меняет фоновый буфер и буфер кадра местами
 }
 
 static void InitializeGlutCallbacks()
 {
-    glutDisplayFunc(RenderSceneCB);
+	glutDisplayFunc(RenderSceneCB);
 }
 
 
-int main(int argc, char** argv)
+void main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1024, 768);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+	glutInit(&argc, argv); //инициализация GLUT
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
-    InitializeGlutCallbacks();
+	glutInitWindowSize(1024, 768);//здесь задаются параметры окна 
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01");//создание окна 
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	InitializeGlutCallbacks();
 
-    glutMainLoop();
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //устанавление цвета, который будет использован во время очистки буфера кадра
 
-    return 0;
+	glutMainLoop(); //передача контроля GLUT'у
 }
