2808cff96f5d1b44a4a2620ebd483b6729cfe83d
Ilyas <gareev_ilyas@list.ru>
2022-04-22 21:06:50 +0500

    ред. ком.

--- 1/Source.cpp
+++ 1/Source.cpp
@@ -1,6 +1,6 @@
 #include <GL/freeglut.h>
 
-static void RenderSceneCB()
+static void RenderSceneCB() //функция отображения на экран
 {
 	glClear(GL_COLOR_BUFFER_BIT);// очистка буфера кадра
 	glutSwapBuffers();//функция, которая меняет фоновый буфер и буфер кадра местами
@@ -8,18 +8,18 @@ static void RenderSceneCB()
 
 static void InitializeGlutCallbacks()
 {
-	glutDisplayFunc(RenderSceneCB);
+	glutDisplayFunc(RenderSceneCB); //функция обратного вызова(отрисовка 1 кадра)
 }
 
 
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv); //инициализация GLUT
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //настраиваются некоторые опции GLUT
 
 	glutInitWindowSize(1024, 768);//здесь задаются параметры окна 
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 01");//создание окна 
+	glutCreateWindow("Tutorial 01");//создание окна с заголовком
 
 	InitializeGlutCallbacks();
 
